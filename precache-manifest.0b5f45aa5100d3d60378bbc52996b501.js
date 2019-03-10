04488705b457b41df774fc05f7673082bb104b67
Alex <alex.reardon14@gmail.com>
2019-03-10 19:58:47 -0400

    Updates

---
+++ precache-manifest.0b5f45aa5100d3d60378bbc52996b501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a4078c09964e4295da5",
+    "url": "/ppc-industry-benchmarks/static/js/runtime~main.6a4078c0.js"
+  },
+  {
+    "revision": "1d2c03dae6f8e7715d0b",
+    "url": "/ppc-industry-benchmarks/static/js/main.1d2c03da.chunk.js"
+  },
+  {
+    "revision": "4bf20628d05b7cf80fde",
+    "url": "/ppc-industry-benchmarks/static/js/2.4bf20628.chunk.js"
+  },
+  {
+    "revision": "1d2c03dae6f8e7715d0b",
+    "url": "/ppc-industry-benchmarks/static/css/main.f37022d3.chunk.css"
+  },
+  {
+    "revision": "3bcd76ed36eda639a6807f1c173387fc",
+    "url": "/ppc-industry-benchmarks/index.html"
+  }
+];
